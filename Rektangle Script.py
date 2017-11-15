2a31dac3d9da2c9ce741426871dc58249db6b4c8
Lomovskoy <lomovskoy.kirill@yandex.ru>
2017-11-15 21:00:06 +0200

    3й урок отрисовка линий между отрендеренными точками, образование полдигонов и заливка их рандомным цветом

--- Rektangle Script.py
+++ Rektangle Script.py
@@ -1,37 +1,36 @@
 # -*- coding: utf-8 -*-
+import random
 from PIL import Image
+import re
 
-scr_x = 200
-scr_y = 200
-
-img = Image.new('RGB', (scr_x+1,scr_y+1), "black")
-canvas = img.load()
+scr_x = 800  # Ширина картинки
+scr_y = scr_x  # Высота картинки
+img = Image.new('RGB', (scr_x, scr_y), 'black')  # Создадим картинку с черным цветом фона
+canvas = img.load()  # Через эту переменную у нас есть доступ к пикселям картинки
 
 
 class Point(object):
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def show(self, color = None):
-        canvas[self.x, scr_y - self.y] = color or (255, 255, 255)
+    def show(self, color=None):
+        canvas[self.x, scr_y-self.y] = color or (255, 255, 255)
 
     def copy(self):
         return Point(self.x, self.y)
 
-        
-points = [Point(20, 30),Point(120, 60),Point(70, 130)]
 
-for p in points:
-    p.show()
+def zero_div(a, b):
+    return float(a)/b if b else 0
 
-def triang(coords):
-    a,b,c = sorted(coords, key = lambda p: p.y)
+
+def triangle(coords, color):
+    a, b, c = sorted(coords, key=lambda p: p.y)
     p1 = a.copy()
-    delta_p1 = float(b.x - a.x)/(b.y - a.y)
     p2 = a.copy()
-    delta_p2 = float(c.x - a.x)/(c.y - a.y)
+    delta_p1 = zero_div((b.x - a.x), (b.y - a.y))
+    delta_p2 = zero_div((c.x - a.x), (c.y - a.y))
     for y in (b.y, c.y):
         while p1.y < y:
             if p1.x > p2.x:
@@ -41,16 +40,30 @@ def triang(coords):
                 p3 = p1.copy()
                 x = p2.x
             while p3.x < x:
-                p3.show()
+                p3.show(color)
                 p3.x += 1
             p1.y += 1
-            p1.x += delta_p1
-            p2.show()
             p2.y += 1
+            p1.x += delta_p1
             p2.x += delta_p2
-        delta_p1 = float(c.x - b.x)/(c.y - b.y)
+        delta_p1 = zero_div((c.x - b.x), (c.y - b.y))
 
-
-triang(points)
+half_scr_x = int(scr_x / 2)
+half_scr_y = int(scr_y / 2)
+f = open('face.obj', 'r')
+lines = f.read()
+points = []
+for line in lines.split('\n'):
+    try:
+        v, x, y, z = re.split('\s+', line)
+    except:
+        continue
+    if v == 'v':
+        x = int((float(x) + 1) * half_scr_x)
+        y = int((float(y) + 1) * half_scr_y)
+        points.append(Point(x, y))
+    if v == 'f':
+        color = tuple([random.randint(0, 255)] * 3)
+        triangle([points[int(i.split('/')[0])-1] for i in (x, y, z)], color)
 
 img.show()
